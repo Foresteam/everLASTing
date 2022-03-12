bac7971887ac58cba47015590466a707a3edd4b9
Альфред Шарифуллин <z2o0u0i3vw@gmail.com>
2022-03-12 20:46:32 +0300

    no work

--- XD.py
+++ XD.py
@@ -3,11 +3,13 @@ import os
 import random
 import discord
 from discord.ext import commands
+from discord_components import DiscordComponents, Button, ButtonStyle
 
-
-DISCORD_TOKEN = 'OTUyMTIyNjMyNzA1MzAyNTc4.YixbTQ.Elvy2SdGST89PFfOySNUXb4yxho'
+DISCORD_TOKEN = 'OTUyMTIyNjMyNzA1MzAyNTc4.YixbTQ.e6ab4XkT77c3UR7mYtV6Uurs_2w'
 DISCORD_GUILD = '906579056210899004'
-client = discord.Client()
+# client = discord.Client()
+client = commands.Bot(command_prefix='$')
+
 
 @client.event
 async def on_ready():
@@ -21,35 +23,19 @@ async def on_ready():
         f'{guild.name}(id: {guild.id})'
     )
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    await message.channel.send('Hello!')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
 
     if message.content == '99!':
-        response = random.choice(brooklyn_99_quotes)
         await message.channel.send("hallo")
         await message.channel.send(file=discord.File(r'teor/Внешняя изоляция.doc'))
         doc_file_1 = discord.Embed(
             title="ВНЕШНЯЯ ИЗОЛЯЦИЯ",
             url="https://docs.google.com/document/d/13FWtGK5MoJoNWQthVwYF1Fr90UFBdoyj/edit",
-            file=discord.File(r'teor/Внешняя изоляция.doc'),
+
             description="Лекция 2",
             color=discord.Color.blue(),
             )
@@ -57,7 +43,7 @@ async def on_message(message):
         doc_file_2 = discord.Embed(
             title="РАЗРЯД ВДОЛЬ ПОВЕРХНОСТИ ТВЕРДОГО ДИЭЛЕКТРИКА",
             url="https://docs.google.com/document/d/1MbzOIKNWgiuKKd-dE-ZIx5Nbv7AhZnqg/edit",
-            file=discord.File(r'teor/Внешняя изоляция.doc'),
+
             description="Лекция 3",
             color=discord.Color.blue(),
         )
@@ -65,7 +51,7 @@ async def on_message(message):
         doc_file_3 = discord.Embed(
             title="Учебное пособие",
             url="https://docs.google.com/document/d/1E0e314FPifF9cusxdrcrqJBHUdAT2qc2/edit",
-            file=discord.File(r'teor/Внешняя изоляция.doc'),
+
             description="Все что тебе нужно",
             color=discord.Color.blue(),
         )
@@ -74,6 +60,7 @@ async def on_message(message):
                               url="https://docs.google.com/presentation/d/1CEDW9hArxB_iVJHoWZgZzj7ob4Jwgzxx/edit#slide=id.p1",
                               description="ВНЕШНЯЯ ИЗОЛЯЦИЯ  ЛЭП И ОРУ(призентация)",
                               color=discord.Color.blue(),
+
                               )
         ptt_file_1.set_thumbnail(url="https://i.imgur.com/axLm3p6.jpeg")
 
@@ -84,12 +71,64 @@ async def on_message(message):
                               )
         ptt_file_2.set_thumbnail(url="https://i.imgur.com/axLm3p6.jpeg")
 
+        await message.channel.send(embed=doc_file_1,
+                                   omponents=[
+                                       Button(style=ButtonStyle.green, label="Accept", emoji="🎄"),
+                                       Button(style=ButtonStyle.red, label="Decline", emoji="🧨"),
+                                       Button(style=ButtonStyle.URL, label="test", url="https://www.google.com")
+                                   ]
+                                   )
+        # response = await client.wait_for("button_click")
+        # if response.channel == message.channel:
+        #     if response.component.label == "text1":
+        #         await response.respond(content="Great!")
+        #     else:
+        #         await response.respond(
+        #             embed=discord.Embed(title="Are you sure?"),
+        #             components=[
+        #                 Button(style=ButtonStyle.green, label="Yes"),
+        #                 Button(style=ButtonStyle.red, label="No"),
+        #                 Button(style=ButtonStyle.blue, label="I'll think...", emoji="🙄"),
+        #
+        #             ]
+        #         )
+
         await message.channel.send(embed=doc_file_1)
         await message.channel.send(embed=doc_file_2)
         await message.channel.send(embed=doc_file_3)
         await message.channel.send(embed=ptt_file_1)
         await message.channel.send(embed=ptt_file_2)
 
+
+@client.command()
+async def start(ctx):
+    start_embed = discord.Embed(
+        title="Добро пожаловать\nПора учиться играя",
+        description="С чего начнем",
+        color=discord.Color.blue(),
+        )
+    await ctx.channel.send(embed=start_embed,
+                           omponents=[
+                               Button(style=ButtonStyle.green, label="ВНЕШНЯЯ ИЗОЛЯЦИЯ", emoji="🎄"),
+                               Button(style=ButtonStyle.red, label="РАЗРЯД ВДОЛЬ ПОВЕРХНОСТИ ТВЕРДОГО ДИЭЛЕКТРИКА", emoji="🧨"),
+                               Button(style=ButtonStyle.blue, label="Учебное пособие", emoji="🎄")
+                           ]
+                           )
+
+    response = await client.wait_for("button_click")
+    if response.channel == ctx.channel:
+        if response.component.label == "text1":
+            await response.respond(content="Great!")
+        else:
+            await response.respond(
+                embed=discord.Embed(title="Are you sure?"),
+                components=[
+                    Button(style=ButtonStyle.green, label="Yes"),
+                    Button(style=ButtonStyle.red, label="No"),
+                    Button(style=ButtonStyle.blue, label="I'll think...", emoji="🙄"),
+
+                ]
+            )
 
 
 client.run(DISCORD_TOKEN)
