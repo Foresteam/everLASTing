ac5fe2e0042ba4fd73c977608e7551f07f8c360d
Foresteam <heler1141@gmail.com>
2022-03-12 18:21:52 +0300

    Test, progress

--- src/Test.py
+++ src/Test.py
@@ -1,9 +1,24 @@
 # https://www.youtube.com/watch?v=c2ZGE5Fpu9E
-from xml.etree.ElementTree import ElementTree as ET
+from xml.etree.ElementTree import ElementTree as ET, Element
 
 class Test:
-    def load(self, filename):
-        tree = ET.parse(filename)
-        
+    class Msg:
+        def __init__(self, text: str = '', attachments: list[str] = [], embeddeds: list[str] = []):
+            self.text = text
+            self.attachments = attachments
+            self.embeddeds = embeddeds
+    class Unit:
+        def __init__(self, beforeSend, msg):
+            self.beforeSend: list[Test.Msg] = beforeSend or []
+            self.msg: Test.Msg = msg or Test.Msg()
+    def load(self, filename: str):
+        xml: Element = ET(file=filename).getroot()
+        self.mix = 'mix' in xml.attrib
+        self.name = xml.attrib['name']
+        self.theory = { 'beforeSend': [], 'self': Test.Msg() }
+
+
     def __init__(self, filename: str):
         self.load(filename)
+
+Test('test.xml')
