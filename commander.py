fb8c227722d501ee95ede187081cae76465498c4
Foresteam <heler1141@gmail.com>
2022-03-12 13:35:05 +0300

    Working command parsing

--- commander.py
+++ commander.py
@@ -9,3 +9,21 @@ class Page(Enum):
     MENU = 0
     GAME = 1
 
+def Help(args, refwith, msg):
+    print(args, refwith)
+
+parser = ArgParser()
+commands = [
+    Command(
+        ['help', '?'],
+        [ { 'type': 'string', 'name': 'arg' } ],
+        '',
+        Help
+    )
+]
+cmd: Command
+try:
+    cmd, args, refwith = parser.parse(input(), commands)
+    cmd.execute(args=args, refwith=refwith, msg=None)
+except:
+    pass
